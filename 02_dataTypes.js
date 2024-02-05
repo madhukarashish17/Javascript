bf6a460713df5cf3c367d4566e1f1e440dfd076d
Ashish Madhukar <madhukar.ashish17@gmail.com>
2024-02-05 13:14:21 +0530

    Different data types

---
+++ 02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict;"  //treat JS codes as newer version 
+
+//alert(2+2); //we are using node not browser
+
+
+
+//Premitive data types
+
+//number => 2 to the power 53
+//string =""
+//null = standalone value
+//boolean = tre/false
+//undefined
+//bigint
+//symbol = unique
+
+//object
+
+console.log(typeof "ashish");
+console.log(typeof null);
+console.log(typeof undefined);
