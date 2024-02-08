05657f69746c36d7e9145bac453c60c8a11cd225
Ashish Madhukar <madhukar.ashish17@gmail.com>
2024-02-08 14:06:10 +0530

    Non primitive

---
+++ Datatype_summary.js
@@ -0,0 +1,26 @@
+// there are basically 2 types of data types categories... PRIMITIVE and NON-PRIMITIVE
+
+/* PRIMITIVE---call by value...meaning ki isme jo original data hai uska reference nhi dia jata hai... uske value ka copy bna ke dia jata hai
+so that usko baad mai naye location pe change kia ja sake*/
+
+//types: String, Number, NULL, Boolean, Undefined, symbol, BigInt
+
+//symbol: used to make a data unique.
+//undefined: means variable is declared, memory space is declared but there is no value in the assigned variable/memory space
+
+/******************************************************************************************************************************/
+
+/*Reference(NON-PRIMITIVE) */
+
+// Array, Objects, Functions
+
+const hero = ["iron man", "Dr.strange","Hulk"];    // Array
+
+let myObj = {                                      // Object
+    name: "Ashish"
+    UserAge: 30 
+}
+
+Const myFunction = function(){                    // Function
+    console.log("Hello world");
+}
